4d240911c06bb9415bdcfd5b755f48f144ad1447
FesalBadday <fesal.n04@hotmail.com>
2024-05-24 15:27:35 -0600

    update

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,8 @@
 // service-worker.js
 
 self.addEventListener('push', function(event) {
+  console.log('Push event received:', event);
+
   const data = event.data ? event.data.json() : {};
   const options = {
       body: data.body || 'Default body',
@@ -12,30 +14,21 @@ self.addEventListener('push', function(event) {
       self.registration.showNotification(data.title || 'Default title', options)
   );
 
-  event.waitUntil(
-      (async function() {
-          const clients = await self.clients.matchAll();
-          if (clients.length === 0) {
-              // If there are no active clients, play sound
-              const soundUrl = 'notification-sound.mp3';
-              const audio = new Audio(soundUrl);
-              audio.play();
-          } else {
-              // Send a message to clients to play sound
-              clients.forEach(client => {
-                  client.postMessage({
-                      action: 'playSound',
-                      soundUrl: 'notification-sound.mp3'
-                  });
-              });
-          }
-      })()
-  );
+  // Send a message to all clients to play the sound
+  self.clients.matchAll().then(function(clients) {
+      clients.forEach(function(client) {
+          console.log('Sending message to client:', client);
+          client.postMessage({
+              action: 'playSound',
+              soundUrl: 'notification-sound.mp3'
+          });
+      });
+  });
 });
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();
   event.waitUntil(
-      clients.openWindow(event.notification.data.url || 'https://fesal.me/push/')
+      clients.openWindow(event.notification.data.url || '/')
   );
 });
